f3072263a42e4e55c5412b0b3d4f326a586aa83b
salimxfardeheb <salimfardeheb442@gmail.com>
2024-07-02 14:57:33 +0100

    Managing the status with useState

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React,{useState} from 'react'
+import TodoList from './components/TodoList';
+import TodoForm from './components/TodoForm';
 import './App.css';
+const App = () => {
+  const [todos, setTodos] = useState([
+    {id: 1, text: 'learn react', completed : false },
+    {id: 2, text: 'build a todo app', completed : false }
+  ]) 
+  const addTodo = text => { 
+    const newTodos = [...todos, {id: Date.now(), text , completed: false}]
+    setTodos(newTodos)
+  }
 
-function App() {
+  const toggleComplete = id => {
+    const newTodos = todos.map(todo => todo.id === id ? {...todo, completed : !todo.completed} : todo)
+    setTodos(newTodos)
+  }
+
+  const deleteTodo = id => {
+    const newTodos = todos.filter(todo => todo.id != id)
+    setTodos(newTodos)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>Todo List</h1>
+      <TodoForm addTodo={addTodo}/>
+      <TodoList todos={todos} toggleComplete={toggleComplete} deleteTodo={deleteTodo}/>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
