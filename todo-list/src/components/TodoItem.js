cc5749d260845c0d81828adaed46651ea7cbf333
salimxfardeheb <salimfardeheb442@gmail.com>
2024-07-06 14:02:28 +0100

    fix bug of deleting tasks

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React from "react";
 import { MdDelete } from "react-icons/md";
 
-const TodoItem = ({ todo, toggleComplete, deleteTodo}) => {
+const TodoItem = ({ todo, toggleComplete, deleteTodo }) => {
   return (
-    <li className='flex justify-between items-start bg-white p-4 rounded-md shadow-md'>
-      <span style={{textDecoration: todo.completed ? 'line-trough' : 'none'}}
-      onClick={() => toggleComplete(todo.id)} className='flex gap-3'>
-        <input type='checkbox' className='w-6 accent-green-600 cursor-pointer'/>
+    <li className="flex justify-between items-start bg-white p-4 rounded-md shadow-md">
+      <span onClick={() => toggleComplete(todo.id)} className="flex gap-3">
+        <input
+          type="checkbox"
+          className="w-6 accent-green-600 cursor-pointer"
+        />
         {todo.text}
       </span>
-      <button onClick={() => deleteTodo(todo.id)} className='text-3xl text-red-600 hover:text-red-700'><MdDelete /></button>
+      <button
+        onClick={() => deleteTodo(todo.id)}
+        className="text-3xl text-red-600 hover:text-red-700"
+      >
+        <MdDelete />
+      </button>
     </li>
-  )
-}
+  );
+};
 
-export default TodoItem
+export default TodoItem;
